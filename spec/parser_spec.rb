0c5dcaa9f75a1034d8b0b88ab0eef6737c8140fb
Richill Tamakloe <richill.tamakloe@gmail.com>
2020-01-07 23:37:26 +0000

    created the spec file and wrote failing test

---
+++ spec/parser_spec.rb
@@ -0,0 +1,10 @@
+require './spec/spec_helper'
+require "./lib/parser.rb"
+
+describe Parser do
+  it 'can return an array of webpages with most page views ordered from most pages views to less page views' do
+  end
+
+  it 'can return an array of webpages with most unique page views also ordered' do
+  end
+end
