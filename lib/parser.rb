5d84615be87a905fadef7e8f48fd7ab103db1ab3
Richill Tamakloe <richill.tamakloe@gmail.com>
2020-01-07 23:58:13 +0000

    created spec and method for most_unique_page_views

--- lib/parser.rb
+++ lib/parser.rb
@@ -22,6 +22,17 @@ class DataCruncher
   end
 
   def most_unique_page_views
-    # write some code
+    data = File.read('webserver.log')
+    split_data = data.split("\n/") # split_data = array
+    split_data[0] = split_data[0].sub('/', '')
+    split_array = split_data.map { |line| line.split(' ') }
+    sorted_unique_views = split_array.map { |h| h.to_a }.uniq.map { |k, v| { k => v } }
+
+    # Number of webpages with most unique page views also ordered
+    sorted_unique_views.group_by do |h|
+      h.to_a.first.first
+    end.map do |k, v|
+      [k, v.count]
+    end.sort_by(&:first).reverse
   end
 end
