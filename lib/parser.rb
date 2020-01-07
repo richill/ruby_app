de1be2a4e8381b614f2b20d00a71a59fa52b77c0
Richill Tamakloe <richill.tamakloe@gmail.com>
2020-01-07 23:49:44 +0000

    completed number_of_visits and test in spec

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,5 +2,22 @@ require 'open-uri'
 require 'pry'
 
 class DataCruncher
-  
+  def number_of_visits
+    data = File.read('webserver.log')
+    split_data = data.split("\n/") # split_data = array
+    split_data[0] = split_data[0].sub('/', '')
+    split_array = split_data.map { |line| line.split(' ') }
+    container = Hash.new(0) # empty
+
+    split_array.each do |item|
+      container[item[0]] += 1
+    end
+
+    sorted_data = container.sort_by { |_k, v| v }.reverse
+
+    # Number of page visits
+    sorted_data.each do |k, v|
+      puts "#{k} has #{v} visits"
+    end
+  end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,6 +3,8 @@ require "./lib/parser.rb"
 
 describe DataCruncher do
   it 'can return an array of webpages with most page views ordered from most pages views to less page views' do
+    expected_result = [["about/2", 90], ["contact", 89], ["index", 82], ["about", 81], ["help_page/1", 80], ["home", 78]]
+    expect(DataCruncher.new.number_of_visits).to eq expected_result
   end
 
   it 'can return an array of webpages with most unique page views also ordered' do
